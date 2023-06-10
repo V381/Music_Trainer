1acc10b8aed7db2bbf07c192494d83edd5ea825f
Pavle Paunovic <paavlee@yahoo.com>
2023-06-11 01:12:31 +0200

    Mobile layout

---
+++ app/mobileLayout.js
@@ -0,0 +1,16 @@
+(() => {
+
+    
+    // if(screen.availHeight > screen.availWidth){
+    //     const header = document.getElementsByTagName('header')[0];
+    //     header.innerHTML = '<h2>Please rotate your mobile to start';
+    //     alert("Please rotate your mobile to Landscape!");
+    // }
+
+    // window.addEventListener("orientationchange", () => {
+    //     if(screen.availHeight !== screen.availWidth){
+    //         return document.body.style.visibility = 'initial';
+    //     }
+    //     document.body.style.visibility = 'hidden';
+    // }, false);
+})();
